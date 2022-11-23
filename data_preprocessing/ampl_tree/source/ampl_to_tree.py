2b6e8b0f8c920539f8235f5344250c213e58d62d
BoGGoG <marco.knipfer@me.com>
2022-11-23 15:48:42 +0100

    Tree expand and contract round trip working
    I don't think it's fully working though.
    `contract_tree` will probably not work for all depths.

--- data_preprocessing/ampl_tree/source/ampl_to_tree.py
+++ data_preprocessing/ampl_tree/source/ampl_to_tree.py
@@ -1,6 +1,7 @@
 from nltk.tree import Tree
 import sympy as sp
 from icecream import ic
+import copy
 
 sqampls_file = "data.nosync/QED_amplitudes_TreeLevel_2to2.txt"
 
@@ -30,12 +31,14 @@ operators = {
 }
 
 
-def ampl_to_tree(arr):
+def ampl_to_tree(arr, remove_hybrid_parentheses=False):
     """convert hybrid prefix amplitude to nltk.Tree
     Hybrid prefix is either normal prefix or prefix where there are products/sums with
     arbitrary number of arguments. They work like this:
         ["Prod(", "a1", "a2", "a3", ")"]
-
+    Watch out, if you `remove_hybrid_parentheses` and then do the round-trip with
+    `tree_to_prefix` with `hybrid=False`, then the result is wrong, because
+    there will be things like [Prod(, 1, 2, 3, )] --> [Prod, 1, 2, 3]
     """
 
     if len(arr) == 1:
@@ -50,12 +53,14 @@ def ampl_to_tree(arr):
     if number_of_args == -1:
         bracket_pos = next_bracket_pos(arr, op_pos)
         func_and_args = arr[op_pos:bracket_pos]
+        if remove_hybrid_parentheses:
+            func_and_args[0] = func_and_args[0][:-1]
         foo = func_to_tree(func_and_args)
         arr_new = arr[:op_pos] + [foo] + arr[bracket_pos+1:]
     else:
         foo = func_to_tree(arr[op_pos:op_pos+number_of_args+1])
         arr_new = arr[:op_pos] + [foo] + arr[op_pos+number_of_args+1:]
-    return ampl_to_tree(arr_new)
+    return ampl_to_tree(arr_new, remove_hybrid_parentheses=remove_hybrid_parentheses)
 
 
 def next_bracket_pos(expr_arr, op_pos):
@@ -93,19 +98,74 @@ def func_to_tree(arr):
     return Tree(func, args)
 
 
-def tree_to_prefix(tree):
-    """converts a tree to an array in prefix notation """
+def tree_to_prefix(tree, hybrid=False):
+    """converts a tree to an array in prefix notation.
+    Automatically detects hybrid prefix if an operator is like `Prod(` )
+    If operators don't have parentheses, you can still go to hybrid prefix with
+    `hybrid=True`. Default is `False`
+    """
     arr = []
-    arr.append(tree.label())
+    node = tree.label()
+    if hybrid and (node in ["Sum", "Prod"]) and (len(tree)>2):
+        node = node + "("  # )
+    arr.append(node)
     for i in range(len(tree)):
         if isinstance(tree[i], Tree):
-            arr = arr + tree_to_prefix(tree[i])
+            arr = arr + tree_to_prefix(tree[i], hybrid=hybrid)
         else:
             arr.append(tree[i])
 
-    if tree.label()[-1] == "(":  # )
+    if node[-1] == "(":  # )
         arr.append(")")
     return arr
+
+
+def expand_tree(tree):
+    """
+    If the tree has Prod or Sum nodes with more than 2 arguments
+    --> expand them to only have 2
+    """
+    if not isinstance(tree, Tree):
+        return tree
+    node = tree.label()
+    nodes_considered = ["Sum", "Sum(", "Prod", "Prod("]  # ))
+    if node[-1] == "(":  # )
+        node = node[:-1]
+        tree.set_label(node)
+    if (node in nodes_considered) and (len(tree) > 2):
+        leaves = [expand_tree(t) for t in tree[1:]]
+        tree[0] = expand_tree(tree[0])
+        tree[1] = expand_tree(Tree(node, leaves))
+        del tree[2:]
+    else:
+        tree[0:] = [expand_tree(t) for t in tree[0:]]
+    return tree
+
+
+def contract_tree(tree, runs=0, add_opening_bracked=True):
+    """inverse of `expand_tree`, not fully tested.
+
+    Note: This implementation is not fully tested and probably not working 100%.
+    One problem was that it didn't work at all depths.
+    Right now my workaround is that I just run it three times and hope all
+    cases are caught, but I don't think so.
+    """
+
+    if runs>2:
+        return tree
+    if not isinstance(tree, Tree):
+        return tree
+    node = tree.label()
+    nodes_considered = ["Sum", "Sum(", "Prod", "Prod("]  # ))
+    if (node in nodes_considered) and (isinstance(tree[1], Tree)) and (tree[1].label() == node):
+        node = node + "("  # )
+        tree.set_label(node)
+        subtree = contract_tree(tree[1])
+        # del[tree[1:]]
+        tree[0:] = tree[0:1] + subtree[0:]
+    else:
+        tree[0:] = [contract_tree(t) for t in tree[0:]]
+    return contract_tree(tree, runs=runs+1)
 
 
 if __name__ == "__main__":
--- data_preprocessing/ampl_tree/tests/test_ampl_to_tree.py
+++ data_preprocessing/ampl_tree/tests/test_ampl_to_tree.py
@@ -10,7 +10,7 @@ parent = os.path.dirname(current)
 sys.path.append(parent)
 
 import sympy as sp
-from source.ampl_to_tree import ampl_to_tree, rightmost_operator_pos, func_to_tree, tree_to_prefix
+from source.ampl_to_tree import ampl_to_tree, rightmost_operator_pos, func_to_tree, tree_to_prefix, expand_tree, contract_tree
 
 
 def test_func_to_tree():
@@ -40,14 +40,24 @@ def test_rightmost_operator_pos():
 
 def test_hybrid():
     arr = ["Prod(", "-1/2", "a", "c", ")"]  # )
-    res = ampl_to_tree(arr)
+    res = ampl_to_tree(arr, remove_hybrid_parentheses=False)
     assert res == Tree("Prod(", ["-1/2", "a", "c"])  # )
 
     arr = ["Prod(", "-1/2", "a", "c", "Pow", "e", "2", ")"]  # )
-    res = ampl_to_tree(arr)
-    ic(res)
-    res.pretty_print()
+    res = ampl_to_tree(arr, remove_hybrid_parentheses=False)
     assert res == Tree("Prod(", ["-1/2", "a", "c", Tree("Pow", ["e", "2"])])  # )
+
+    arr = ["Prod(", "-1/2", "a", "c", ")"]  # )
+    res = ampl_to_tree(arr, remove_hybrid_parentheses=True)
+    assert res == Tree("Prod", ["-1/2", "a", "c"])  # )
+
+    arr = ["Prod(", "-1/2", "a", "c", "Pow", "e", "2", ")"]  # )
+    res = ampl_to_tree(arr, remove_hybrid_parentheses=True)
+    assert res == Tree("Prod", ["-1/2", "a", "c", Tree("Pow", ["e", "2"])])  # )
+
+    arr = ["Prod(", "-1/2", "a", "c", "Sum(", "1", "2", "3", ")", ")"]  # )
+    res = ampl_to_tree(arr, remove_hybrid_parentheses=True)
+    assert res == Tree("Prod", ["-1/2", "a", "c", Tree("Sum", ["1", "2", "3"])])  # )
 
 
 def test_ampl_to_tree():
@@ -61,18 +71,14 @@ def test_ampl_to_tree():
 
     arr = ["Prod", "-1/2", "Prod", "e", "2"]
     res = ampl_to_tree(arr)
-    ic(res)
     assert res == Tree("Prod", ["-1/2", Tree("Prod", ["e", "2"])])
 
     arr = ["Prod", "-1/2", "gamma", "alpha_1", "alpha_2", "alpha_3"]
     res = ampl_to_tree(arr)
-    ic(res)
     assert res == Tree("Prod", ["-1/2", Tree("gamma", ["alpha_1", "alpha_2", "alpha_3"])])
 
     arr = ["Prod", "Pow", "i", "-1/2", "gamma", "alpha_1", "alpha_2", "alpha_3"]
     res = ampl_to_tree(arr)
-    ic(res)
-    res.pretty_print()
     # assert res == Tree("Prod", ["-1/2", Tree("gamma", ["alpha_1", "alpha_2", "alpha_3"])])
 
 
@@ -80,7 +86,6 @@ def test_full_ampls():
     arr = "Prod(,-1/2,i,Pow,e,2,Pow,Sum(,Pow,m_e,2,Prod,-1,s_13,Prod,1/2,reg_prop,),-1,gamma,alpha_4,alpha_2,alpha_0,gamma,alpha_4,alpha_3,alpha_1,ee,i_0,alpha_0,(p_1)_u,ee,i_2,alpha_1,(p_2)_u,ee^(*),i_3,alpha_2,(p_3)_u,ee^(*),i_1,alpha_3,(p_4)_u,)"
     arr = arr.split(",")
     res = ampl_to_tree(arr)
-    res.pretty_print()
 
     arr = "Prod(,1/9,i,Pow,e,2,Pow,Sum(,Pow,m_b,2,Prod,-1,s_11,Prod,2,s_13,Prod,-1,s_33,Prod,-1,reg_prop,),-1,Sum(,Prod(,m_b,gamma,alpha_12,alpha_3,alpha_6,gamma,alpha_13,alpha_6,alpha_0,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_3,(p_1)_u,b,i_2,alpha_0,(p_2)_v,),Prod(,p_1,alpha_11,gamma,alpha_11,alpha_7,alpha_8,gamma,alpha_12,alpha_4,alpha_7,gamma,alpha_13,alpha_8,alpha_1,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_4,(p_1)_u,b,i_2,alpha_1,(p_2)_v,),Prod(,-1,p_3,alpha_11,gamma,alpha_11,alpha_9,alpha_10,gamma,alpha_12,alpha_5,alpha_9,gamma,alpha_13,alpha_10,alpha_2,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_5,(p_1)_u,b,i_2,alpha_2,(p_2)_v,),),)"
     arr = arr.split(",")
@@ -107,6 +112,99 @@ def test_tree_to_prefix():
     arr = "Prod(,1/9,i,Pow,e,2,Pow,Sum(,Pow,m_b,2,Prod,-1,s_11,Prod,2,s_13,Prod,-1,s_33,Prod,-1,reg_prop,),-1,Sum(,Prod(,m_b,gamma,alpha_12,alpha_3,alpha_6,gamma,alpha_13,alpha_6,alpha_0,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_3,(p_1)_u,b,i_2,alpha_0,(p_2)_v,),Prod(,p_1,alpha_11,gamma,alpha_11,alpha_7,alpha_8,gamma,alpha_12,alpha_4,alpha_7,gamma,alpha_13,alpha_8,alpha_1,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_4,(p_1)_u,b,i_2,alpha_1,(p_2)_v,),Prod(,-1,p_3,alpha_11,gamma,alpha_11,alpha_9,alpha_10,gamma,alpha_12,alpha_5,alpha_9,gamma,alpha_13,alpha_10,alpha_2,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_5,(p_1)_u,b,i_2,alpha_2,(p_2)_v,),),)"
     arr = arr.split(",")
     tree = ampl_to_tree(arr)
-    round_trip = tree_to_prefix(tree)
+    round_trip = tree_to_prefix(tree, hybrid=True)
     assert arr == round_trip
+
+def test_tree_to_prefix_hybrid():
+    arr = ["Prod(", "-1/2", "a", "c", ")"]  # )
+    tree = ampl_to_tree(arr, remove_hybrid_parentheses=True)
+    round_trip = tree_to_prefix(tree)
+    round_trip = tree_to_prefix(tree, hybrid=True)
+    assert round_trip == arr
     
+
+def test_round_trips():
+    arr = ["Prod(", "-1/2", "a", "c", "d", ")"]  # )
+    tree = ampl_to_tree(arr, remove_hybrid_parentheses=True)
+    round_trip = tree_to_prefix(tree, hybrid=True)
+    assert round_trip == arr
+    
+    arr_2 = ["Prod", "-1/2", "Prod", "a", "Prod", "c", "d"]  # )
+    tree_2 = ampl_to_tree(arr_2, remove_hybrid_parentheses=True)
+    round_trip = tree_to_prefix(tree_2, hybrid=True)
+    ic(arr)
+    ic(arr_2)
+    ic(round_trip)
+    # assert round_trip == arr
+
+def test_expand_tree():
+    arr = ["Prod(", "-1/2", "a", "c", "d", ")"]  # )
+    tree = ampl_to_tree(arr, remove_hybrid_parentheses=True)
+    tree_1 = ampl_to_tree(arr, remove_hybrid_parentheses=False)
+    print("tree")
+    tree.pretty_print()
+    expand_tree(tree).pretty_print()
+    print("tree1")
+    tree_1.pretty_print()
+    expand_tree(tree_1).pretty_print()
+
+    print("tree2")
+    arr_2 = ["Prod", "-1/2","Prod", "a", "Prod", "c", "d"]  # )
+    tree_2 = ampl_to_tree(arr_2, remove_hybrid_parentheses=True)
+    ic(expand_tree(tree_2))
+    tree_2.pretty_print()
+
+    print("tree3")
+    arr_3 = ["Prod(", "-1/2", "a", "c", "Sum(", "1", "2", "3", ")", ")"]  # )
+    tree_3 = ampl_to_tree(arr_3, remove_hybrid_parentheses=True)
+    tree_3.pretty_print()
+    expand_tree(tree_3).pretty_print()
+
+    arr_4 = "Prod(,1/9,i,Pow,e,2,Pow,Sum(,Pow,m_b,2,Prod,-1,s_11,Prod,2,s_13,Prod,-1,s_33,Prod,-1,reg_prop,),-1,Sum(,Prod(,m_b,gamma,alpha_12,alpha_3,alpha_6,gamma,alpha_13,alpha_6,alpha_0,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_3,(p_1)_u,b,i_2,alpha_0,(p_2)_v,),Prod(,p_1,alpha_11,gamma,alpha_11,alpha_7,alpha_8,gamma,alpha_12,alpha_4,alpha_7,gamma,alpha_13,alpha_8,alpha_1,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_4,(p_1)_u,b,i_2,alpha_1,(p_2)_v,),Prod(,-1,p_3,alpha_11,gamma,alpha_11,alpha_9,alpha_10,gamma,alpha_12,alpha_5,alpha_9,gamma,alpha_13,alpha_10,alpha_2,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_5,(p_1)_u,b,i_2,alpha_2,(p_2)_v,),),)"
+    arr_4 = arr_4.split(",")
+    tree_4 = ampl_to_tree(arr_4)
+    expand_tree(tree_4).pretty_print()
+
+
+def test_contract_tree():
+    print("---")
+    arr = ["Prod(", "-1/2", "a", "c", "d", ")"]  # )
+    tree = ampl_to_tree(arr, remove_hybrid_parentheses=True)
+    tree.pretty_print()
+    tree_expanded = expand_tree(tree)
+    tree_expanded.pretty_print()
+    tree_contracted = contract_tree(tree_expanded)
+    tree_contracted.pretty_print()
+
+    print("---")
+    arr_2 = ["Prod", "-1/2","Prod", "a", "Prod", "c", "d"]  # )
+    tree_2 = ampl_to_tree(arr_2, remove_hybrid_parentheses=True)
+    tree_2.pretty_print()
+    tree_2_contracted = contract_tree(tree_2)
+    tree_2_contracted.pretty_print()
+
+    print("---")
+    arr_3 = ["Prod(", "-1/2", "a", "c", "Sum(", "1", "2", "3", ")", ")"]  # )
+    tree_3 = ampl_to_tree(arr_3, remove_hybrid_parentheses=True)
+    tree_3.pretty_print()
+    tree_3_contracted = contract_tree(tree_3)
+    tree_3_contracted.pretty_print()
+    tree_3_round_trip = expand_tree(tree_3_contracted)
+    assert tree_3_round_trip == tree_3
+
+    print("---")
+    print("testing expand contract roundtrip")
+    arr_4 = "Prod(,1/9,i,Pow,e,2,Pow,Sum(,Pow,m_b,2,Prod,-1,s_11,Prod,2,s_13,Prod,-1,s_33,Prod,-1,reg_prop,),-1,Sum(,Prod(,m_b,gamma,alpha_12,alpha_3,alpha_6,gamma,alpha_13,alpha_6,alpha_0,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_3,(p_1)_u,b,i_2,alpha_0,(p_2)_v,),Prod(,p_1,alpha_11,gamma,alpha_11,alpha_7,alpha_8,gamma,alpha_12,alpha_4,alpha_7,gamma,alpha_13,alpha_8,alpha_1,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_4,(p_1)_u,b,i_2,alpha_1,(p_2)_v,),Prod(,-1,p_3,alpha_11,gamma,alpha_11,alpha_9,alpha_10,gamma,alpha_12,alpha_5,alpha_9,gamma,alpha_13,alpha_10,alpha_2,A,i_3,alpha_12,(p_3),A,i_1,alpha_13,(p_4),b^(*),i_0,alpha_5,(p_1)_u,b,i_2,alpha_2,(p_2)_v,),),)"
+    arr_4 = arr_4.split(",")
+    print("base tree")
+    tree_4 = ampl_to_tree(arr_4)
+    tree_4.pretty_print(unicodelines=True)
+    print("expanded tree")
+    tree_4_expanded = expand_tree(tree_4)
+    tree_4_expanded.pretty_print(unicodelines=True)
+    print("roundtrip")
+    tree_4_roundtrip = contract_tree(tree_4_expanded)
+    tree_4_roundtrip.pretty_print(unicodelines=True)
+    assert tree_4 == tree_4_roundtrip
+    
+
