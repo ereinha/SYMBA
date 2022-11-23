29ec3161891a37d1c3b34011b3e87fb22098426c
BoGGoG <marco.knipfer@me.com>
2022-11-23 11:23:09 +0100

    ampl_to_tree working

--- data_preprocessing/ampl_tree/source/ampl_to_tree.py
+++ data_preprocessing/ampl_tree/source/ampl_to_tree.py
@@ -5,47 +5,83 @@ from icecream import ic
 sqampls_file = "data.nosync/QED_amplitudes_TreeLevel_2to2.txt"
 
 
-# the amplitudes are already in hybrid prefix notation since I exported them like this from MARTY.
+# the amplitudes are already in hybrid prefix notation. Now writing a script to convert them to a tree is kind
+# of stupid, because I had them as trees before I converted them.
+# Sadly there are so many steps in between and I don't want to change them now.
 # GOAL: Convert to
 # - tree
 # - prefix
 # Since I think it easier to first go to the tree and then prefix, I'll do the tree here.
 
-# basis functions and how many arguments
-basis_functions = {
-                  "ee": 3,
-                  "ee^(*)": 3
-}
 
 operators = {
+        "ee": 3,
+        "ee^(*)": 3,
         "Prod": 2,
-        "Prod(": -1,
-        "Mul": 2,
-        "Mul(": -1,
+        "Prod(": -1,  # )
         "Pow": 2,
+        "gamma": 3,
+        "Sum": 2,
+        "Sum(": -1,  # )
 }
 
+
 def ampl_to_tree(arr):
+    """convert hybrid prefix amplitude to nltk.Tree"""
+
     if len(arr) == 1:
         return arr[0]
 
     op_pos = rightmost_operator_pos(arr)
-    return 0
+    if (op_pos == -1) | (op_pos == len(arr)):
+        print("something went wrong, operator should not be at end of array")
+
+    op = arr[op_pos]
+    number_of_args = operators[op]
+    if number_of_args == -1:
+        bracket_pos = next_bracket_pos(arr, op_pos)
+        func_and_args = arr[op_pos:bracket_pos]
+        foo = func_to_tree(func_and_args)
+        arr_new = arr[:op_pos] + [foo] + arr[bracket_pos+1:]
+    else:
+        foo = func_to_tree(arr[op_pos:op_pos+number_of_args+1])
+        arr_new = arr[:op_pos] + [foo] + arr[op_pos+number_of_args+1:]
+    return ampl_to_tree(arr_new)
+
+
+def next_bracket_pos(expr_arr, op_pos):
+    pos = op_pos + 1
+    x = expr_arr[pos] 
+    while x != ")":
+        pos = pos + 1
+        x = expr_arr[pos]
+        if pos > len(expr_arr):
+            print("Error in next_bracket_pos")
+            return -1
+    return pos
 
 
 def rightmost_operator_pos(expr, pos=-1):
-    # operators = list(operators_inv.keys()) + ["s+", "s-"] + variables
+    """
+    from expression get the rightmost operator position.
+    The operators are in the global dictionary `operators`.
+
+    Notes:
+        - if `expr[pos]` is already a Tree, then checking if it's in `operators.keys()` fails,
+        thus first check if it's a tree. (Tree not hashable)
+    """
+    if isinstance(expr[pos], Tree):
+        return rightmost_operator_pos(expr, pos-1)
     if expr[pos] in operators.keys():
         return len(expr) + pos
     else:
         return rightmost_operator_pos(expr, pos-1)
 
 
-def gamma_to_tree(arr):
-    """take array [gamma, idx1, idx2, idx3] and return tree"""
-    gam = arr[0]
-    indices = arr[1:]
-    return Tree(gam, indices)
+def func_to_tree(arr):
+    func = arr[0]
+    args = arr[1:]
+    return Tree(func, args)
 
 
 if __name__ == "__main__":
--- data_preprocessing/ampl_tree/tests/test_ampl_to_tree.py
+++ data_preprocessing/ampl_tree/tests/test_ampl_to_tree.py
@@ -10,15 +10,17 @@ parent = os.path.dirname(current)
 sys.path.append(parent)
 
 import sympy as sp
-from source.ampl_to_tree import ampl_to_tree, gamma_to_tree, rightmost_operator_pos
+from source.ampl_to_tree import ampl_to_tree, rightmost_operator_pos, func_to_tree
 
 
-def test_gamma():
+def test_func_to_tree():
     arr = ["gamma", "alpha1", "alpha2", "alpha3"]
-    res = gamma_to_tree(arr)
+    res = func_to_tree(arr)
     res_test = Tree("gamma", ["alpha1", "alpha2", "alpha3"])
-    print(arr)
-    print(res)
+    assert res == res_test
+    arr = ["Prod", "a", "b"]
+    res = func_to_tree(arr)
+    res_test = Tree("Prod", ["a", "b"])
     assert res == res_test
 
 
@@ -28,9 +30,48 @@ def test_finished():
 
 
 def test_rightmost_operator_pos():
-    arr = ["Prod(", "-1/2", "i", "Pow", "e", "2"]
+    arr = ["Prod(", "-1/2", "i", "Pow", "e", "2", ")"]
     res = rightmost_operator_pos(arr)
     assert res == 3
     arr = ["Sum(", "Pow", "m_e", "2","Prod", "-1", "s_13" ,"Prod", "1/2","reg_prop", ")"]
     res = rightmost_operator_pos(arr)
     assert res == 7
+
+
+def test_hybrid():
+    arr = ["Prod(", "-1/2", "a", "b", ")"]  # )
+    res = ampl_to_tree(arr)
+    assert res == Tree("Prod(", ["-1/2", "a", "b"])  # )
+
+    arr = ["Prod(", "-1/2", "a", "b", "Pow", "e", "2", ")"]  # )
+    res = ampl_to_tree(arr)
+    ic(res)
+    res.pretty_print()
+    assert res == Tree("Prod(", ["-1/2", "a", "b", Tree("Pow", ["e", "2"])])  # )
+
+
+def test_ampl_to_tree():
+    arr = ["Prod", "-1/2", "Pow", "e", "2"]
+    res = ampl_to_tree(arr)
+    assert res == Tree("Prod", ["-1/2", Tree("Pow", ["e", "2"])])
+
+    arr = ["Prod", "-1/2", "Sum", "e", "2"]
+    res = ampl_to_tree(arr)
+    assert res == Tree("Prod", ["-1/2", Tree("Sum", ["e", "2"])])
+
+    arr = ["Prod", "-1/2", "Prod", "e", "2"]
+    res = ampl_to_tree(arr)
+    ic(res)
+    assert res == Tree("Prod", ["-1/2", Tree("Prod", ["e", "2"])])
+
+    arr = ["Prod", "-1/2", "gamma", "alpha_1", "alpha_2", "alpha_3"]
+    res = ampl_to_tree(arr)
+    ic(res)
+    assert res == Tree("Prod", ["-1/2", Tree("gamma", ["alpha_1", "alpha_2", "alpha_3"])])
+
+    arr = ["Prod", "Pow", "i", "-1/2", "gamma", "alpha_1", "alpha_2", "alpha_3"]
+    res = ampl_to_tree(arr)
+    ic(res)
+    res.pretty_print()
+    # assert res == Tree("Prod", ["-1/2", Tree("gamma", ["alpha_1", "alpha_2", "alpha_3"])])
+
